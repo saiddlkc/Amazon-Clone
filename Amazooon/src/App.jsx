50c526715bb32c3ce0ef20ec1195debb24ef3bb2
DrCetmi <drcetmi@gmail.com>
2024-02-14 15:53:13 +0100

    homeseite begann

--- Amazooon/src/App.jsx
+++ Amazooon/src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.css";
+import Banner from "./pages/home/Banner";
 
 function App() {
   return (
     <>
       <div>
-        <h1>Amazon hallo durmus siehst du das</h1>
+        <Banner />
       </div>
     </>
   );
---
+++ Amazooon/src/pages/home/Banner.jsx
@@ -0,0 +1,32 @@
+import React, { useRef, useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+
+import "./Banner.css";
+import { Navigation } from "swiper/modules";
+
+const banners = [
+  "https://m.media-amazon.com/images/I/61-t7nfD-rL._SX3000_.jpg",
+  "https://m.media-amazon.com/images/I/71erLF1rQsL._SX3000_.jpg",
+  "https://images-eu.ssl-images-amazon.com/images/G/31/prime/Gateway/2020/May/gaming_3000x1200._CB431281466_.jpg",
+  "https://images-eu.ssl-images-amazon.com/images/G/31/img21/Wireless/WLA/Feb/NC/D21052619_WLA_BAU_GW-heroes_Headsets_FPF_FEB_DesktopTallHero_3000x1200._CB660350658_.jpg",
+  "https://images-eu.ssl-images-amazon.com/images/G/31/img21/Audio/MI/Final/MI_Gw_3000x1200._CB659658858_.jpg",
+];
+
+function Banner() {
+  return (
+    <div className="banner">
+      <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+        {" "}
+        {banners.map((banner, index) => (
+          <SwiperSlide>
+            <img key={index} src={banner} alt="banner" />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+}
+
+export default Banner;
