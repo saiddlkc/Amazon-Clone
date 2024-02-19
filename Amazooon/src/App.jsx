419a36f59243ea43078efc49fff2d72532925ce9
Tairou Mouhamed <tairou97-mouhamed@hotmail.com>
2024-02-19 15:07:39 +0100

    change

--- Amazooon/src/App.jsx
+++ Amazooon/src/App.jsx
@@ -1,13 +1,12 @@
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import "./App.css";
-
 import Layout from "./layout/Layout";
 import Ok from "./pages/Ok";
 
 function App() {
   return (
     <>
-      <Layout></Layout>
+      <Layout />
     </>
   );
 }
