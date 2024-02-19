7b295946a1066b113d239849c4e3859cfd2eab43
saiddlkc <saidalkilic@gmail.com>
2024-02-19 16:01:33 +0100

    said

--- Amazooon/src/App.jsx
+++ Amazooon/src/App.jsx
@@ -1,12 +1,12 @@
 import "./App.css";
-import Login from "./components/Login";
+
 import {
   BrowserRouter,
   Route,
   BrowserRouter as Router,
   Routes,
 } from "react-router-dom";
-import Registration from "./components/Registration";
+
 import RegistrationPage from "./pages/RegistrationPage";
 import LoginPage from "./pages/LoginPage";
 
