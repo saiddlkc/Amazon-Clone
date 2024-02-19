0e8b35740399c490c3faa646e9a05cbe7ed84a2f
saiddlkc <saidalkilic@gmail.com>
2024-02-19 15:58:25 +0100

    durmus

--- Amazooon/src/App.jsx
+++ Amazooon/src/App.jsx
@@ -2,32 +2,25 @@ import { Route, Routes, BrowserRouter } from "react-router-dom";
 import "./App.css";
 
 import HomePage from "./pages/home/HomePage";
-
-
 import RegistrationPage from "./pages/RegistrationPage";
 import LoginPage from "./pages/LoginPage";
 import Layout from "./layout/Layout";
 import Ok from "./pages/Ok";
 
-
 function App() {
   return (
     <>
-      <div className="App">
-        <HomePage />
-      </div>
-
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<HomePage />}></Route>
           <Route path="/nav" element={<Layout />}></Route>
-          <Route path="/" element={<LoginPage />}></Route>
+          <Route path="/Login" element={<LoginPage />}></Route>
           <Route
             path="/RegistrationPage"
             element={<RegistrationPage />}
           ></Route>
         </Routes>
       </BrowserRouter>
-
     </>
   );
 }
